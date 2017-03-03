e0ce993960d588906c145ef52eb1fbae4e1413b9
evemsilva <everton0189@gmail.com>
2017-03-02 22:49:29 -0300

    Montagem da tela de produtos

---
+++ public/js/controllers/ProdutosController.js
@@ -0,0 +1,113 @@
+angular.module('cantina-asser').controller('ProdutosController', function ($scope) {
+
+    $scope.produtos = [{
+            'codigo': 1,
+            'nome': 'Chocolate Bis',
+            'categoria': {
+                'identificador': 1,
+                'descricao': 'Bomboniere'
+            },
+            'preco': 3.59
+        },
+        {
+            'codigo': 2,
+            'nome': 'Coca-Cola 2L',
+            'categoria': {
+                'identificador': 2,
+                'descricao': 'Bebidas'
+            },
+            'preco': 6.49
+        },
+        {
+            'codigo': 1,
+            'nome': 'Chocolate Bis',
+            'categoria': {
+                'identificador': 1,
+                'descricao': 'Bomboniere'
+            },
+            'preco': 3.59
+        },
+        {
+            'codigo': 2,
+            'nome': 'Coca-Cola 2L',
+            'categoria': {
+                'identificador': 2,
+                'descricao': 'Bebidas'
+            },
+            'preco': 6.49
+        },
+        {
+            'codigo': 1,
+            'nome': 'Chocolate Bis',
+            'categoria': {
+                'identificador': 1,
+                'descricao': 'Bomboniere'
+            },
+            'preco': 3.59
+        },
+        {
+            'codigo': 2,
+            'nome': 'Coca-Cola 2L',
+            'categoria': {
+                'identificador': 2,
+                'descricao': 'Bebidas'
+            },
+            'preco': 6.49
+        },
+        {
+            'codigo': 1,
+            'nome': 'Chocolate Bis',
+            'categoria': {
+                'identificador': 1,
+                'descricao': 'Bomboniere'
+            },
+            'preco': 3.59
+        },
+        {
+            'codigo': 2,
+            'nome': 'Coca-Cola 2L',
+            'categoria': {
+                'identificador': 2,
+                'descricao': 'Bebidas'
+            },
+            'preco': 6.49
+        },
+        {
+            'codigo': 1,
+            'nome': 'Chocolate Bis',
+            'categoria': {
+                'identificador': 1,
+                'descricao': 'Bomboniere'
+            },
+            'preco': 3.59
+        },
+        {
+            'codigo': 2,
+            'nome': 'Coca-Cola 2L',
+            'categoria': {
+                'identificador': 2,
+                'descricao': 'Bebidas'
+            },
+            'preco': 6.49
+        },
+        {
+            'codigo': 1,
+            'nome': 'Chocolate Bis',
+            'categoria': {
+                'identificador': 1,
+                'descricao': 'Bomboniere'
+            },
+            'preco': 3.59
+        },
+        {
+            'codigo': 2,
+            'nome': 'Coca-Cola 2L',
+            'categoria': {
+                'identificador': 2,
+                'descricao': 'Bebidas'
+            },
+            'preco': 6.49
+        }
+    ];
+
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,21 @@
-angular.module('cantina-asser',['ngRoute'])
+angular.module('cantina-asser',['ngRoute','smart-table'])
   .config(function($routeProvider, $httpProvider) {
 
     $routeProvider.when('/', {
-      templateUrl: 'partials/principal.html'
+      templateUrl: 'partials/home.html',
+      controller: 'ProdutosController'
+    });
+
+    $routeProvider.when('/sobre', {
+      templateUrl: 'partials/sobre.html'
+    });
+
+    $routeProvider.when('/blog', {
+      templateUrl: 'partials/blog.html'
+    });
+
+    $routeProvider.when('/contato', {
+      templateUrl: 'partials/contato.html'
     });
 
     // $routeProvider.otherwise({redirectTo: '/contatos'});
