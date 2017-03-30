e228594af6740ace82129f7b8ff702bb3942e132
everton.silva <everton.silva@accenture.com>
2017-03-30 17:47:49 -0300

    Preparando login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
 
+require('./config/passport')();
 require('./config/database.js')('mongodb://localhost/cantina');
 
 http.createServer(app).listen(app.get('port'), function () {
