501daf540b7cd247624575e863abf428b7ee41b3
everton.silva <everton.silva@accenture.com>
2017-04-12 17:07:08 -0300

    Incluindo melhorias

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-angular.module('cantina-asser', ['ngRoute', 'ngResource', 'smart-table','datetimepicker'])
+angular.module('cantina-asser', ['ngRoute', 'ngResource', 'smart-table','datetimepicker', 'ui.utils.masks', 'idf.br-filters'])
   .config(function ($routeProvider, $httpProvider) {
 
     // Interceptador de requisicao
