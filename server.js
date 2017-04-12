3aac979840be9084bb758bf67d7daefcbd95a0fe
everton.silva <everton.silva@accenture.com>
2017-04-12 14:38:08 -0300

    Adicionando variavel ambiente

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var app = require('./config/express')();
 
 require('./config/passport')();
-require('./config/database.js')('mongodb://localhost/cantina');
+require('./config/database.js')(process.env.MONGODB_URI || 'mongodb://localhost/cantina');
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express Server escutando na porta ' +
